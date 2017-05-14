badff6255e137fe1bb6b8637a31780c9af536509
new user <aljones15@gmail.com>
2017-05-14 10:31:18 +0000

    moving towards a mock google map

--- src/map/test.js
+++ src/map/test.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+
+export class mockGoogleMap extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    <div>test</div>
+  }
+}
+
+jest.mock('google-map-react', () => { return new mockGoogleMap } );
+
 import Map from './index';
 
 describe('map should', () => {
