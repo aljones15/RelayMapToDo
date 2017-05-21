7f4646d6a4a6fdb2cb3e3de2a09a174ca05e9913
liminal18 <aljones15@gmail.com>
2017-05-21 11:15:58 +0700

    schema nearly complete

--- data/schema.js
+++ data/schema.js
@@ -9,6 +9,17 @@ import {
   FetchCity
 } from './database';
 
+import {
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLString,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLEnumType,
+  GraphQLNonNull,
+  GraphQLSchema
+} from 'graphql';
+
 /***
 * graph QL function that maps Queries to functions that fetch the correct obj
 * function return is destructured to all us acces to the interface ani fields
@@ -39,16 +50,54 @@ const {nodeInterface, nodeField} = nodeDefinitions(
   }
 );
 
-const citiesType = new GraphQLObjectType({
-  name: 'Cities',
-  description: 'An array of Cities'
+const Root = new GraphQLObjectType({
+  name: 'RootQuery',
+  description: 'An array of Cities',
+  fields: () => ({
+    cities: { 
+      type: new GraphQLList(City),
+      resolve: () => FetchCities()
+    }
+    city: {
+      type: City,
+      args: {
+        id: {type: GraphQLInt}
+      },
+      resolve: (source, {id}) => FetchCity(id)
+    }
+  })
 });
 
-const cityType = new GraphQLObjectType({
+const City = new GraphQLObjectType({
   name: 'City',
   description: 'A city to be used on the map',
   fields: () => ({
-    id: globalIdField('City');
-    tdods: FetchToDos(id);
+    id: {type: new GraphQLNonNull(GraphQLInt)},
+    lat: {type: GraphQLFloat},
+    lng: {type: GraphQLFloat},
+    todo:{ 
+      type: new GrapQLList(ToDo),
+      resolve: (city) => FetchToDos(city.id)
+    }
   })
 });
+
+const ToDo = new GraphQLObjectType({
+  name: 'ToDo',
+  description: 'A To Do for a city',
+  fields: () => ({
+    city_id: {type: new GraphQLNonNull(GraphQLInt)},
+    text: {type: GraphQLString},
+    likes: {
+      type: GraphQLInt
+      resolve: (todo) => todo.likes || 0
+    },
+    id: {type: new GraphQLNonNull(GraphQLInt)}
+  })
+});
+
+const Schema = new GraphQLSchema({
+  query: Root
+});
+
+export default Schema;
