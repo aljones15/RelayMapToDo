d273c01d782d72a8ab2b88a7748891fdf79cc564
new user <aljones15@gmail.com>
2017-05-21 10:30:13 +0000

    babel update relay babel is working

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Map from './map/index.jsx';
 import Relay from 'react-relay';
+import CityRoute from './routes/cities';
 
 ReactDOM.render(<Map />, document.getElementById('app'));
