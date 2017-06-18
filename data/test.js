aa190a7a7db3b14a8823016643ac66cfdeca35aa
liminal18 <aljones15@gmail.com>
2017-06-18 14:15:16 +0700

    started database tests

--- data/test.js
+++ data/test.js
@@ -1,1 +1,8 @@
+import { FetchCities } from './database.js';
 
+describe("should", () => {
+  it("return 3 cities", () => {
+    const cities = FetchCities();
+    expect(cities.length).toBe(3);
+  })
+})
