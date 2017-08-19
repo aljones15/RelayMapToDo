7d10301b10b73672df54dffd4b884a75453bc5c8
liminal18 <aljones15@gmail.com>
2017-08-19 13:43:08 +0700

    ok using aphroidte styles

--- src/ToDoList/index.jsx
+++ src/ToDoList/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ToDo from '../ToDo/index.jsx';
+import { AddToTitle, style } from './style';
 import {
-  ToDoStyle, 
-  ToDoAddStyle, 
-  ToDoRowStyle,
-  AddToTitle,
-  AddToDoInput,
-  AddToDoSubmit
-} from './style';
-import {QueryRenderer, graphql, createPaginationContainer} from 'react-relay';
+  QueryRenderer, 
+  graphql, 
+  createPaginationContainer
+} from 'react-relay';
 import AddToDo from './CreateToDoMutation.jsx';
 import environment from '../../data/relayEnv';
 import _ from 'lodash';
+import { css } from 'aphrodite';
 
 const rootQuery = graphql`
       query ToDoListQuery(
@@ -33,13 +31,21 @@ class ToDoPage extends React.Component {
     this.setState({text: event.target.value});
   }
   makeToDo(todo, index){
-    return(<li key={`todo?${index}`} style={ToDoAddStyle}>{todo.node.text}</li>);
+    return(
+      <li 
+        key={`todo?${index}`} 
+        className={css(style.toDoRow)}>
+      {todo.node.text}
+      </li>);
   }
   paginateToDo(todos){
     const {count, page} = this.state;
     const pages = _.chunk(todos, count);
+    console.log('to dos and pages');
+    console.log(todos);
+    console.log(pages);
     if(pages.length == 0 || !pages[page]){
-      return null;
+      return [];
     }
     return pages[page].map(this.makeToDo);
   }
@@ -51,37 +57,50 @@ class ToDoPage extends React.Component {
     this.loadMore();
     this.setState((pstate, props) => 
       ({page: pstate.page + 1}))
-
   }
   render(){
     const {todo} = this.props; 
     return(
-      <ul className='ToDoUl' style={ToDoStyle}>
-        <li 
-          style={ToDoAddStyle} 
-          onClick={() => AddToDo(this.props.city_id, this.state.text)}>
-          <h3 style={AddToTitle}>Know Something to do here?</h3>
-          <label htmlFor='addtodo' > To Do:</label>
-          <textarea 
+      <div id='todoHolder' className={css(style.toDo)} >
+        <form className={css(style.toDo)}>
+          <h3  className={css(style.toDoRow, style.selfCenter)}>
+            Know Something to do here?
+          </h3>
+         <textarea 
             id='addtodo' 
             type='text' 
-            style={ToDoRowStyle} 
+            className={css(style.toDoRow)} 
             onChange={this.updateText.bind(this)}
           />
-          <button style={ToDoRowStyle}>Submit</button>
-       </li>
-       {this.paginateToDo.bind(this)(todo.todo.edges)}
-       <li>
-         <button onClick={this.back.bind(this)}>Back</button>
-         <button onClick={this.forward.bind(this)}>Forward</button>
-         <strong>Page: {this.state.page}</strong>
-      </li>
-      
-     </ul>
+          <button 
+            type='button'
+            onClick={() => AddToDo(this.props.city_id, this.state.text)} 
+            className={css(style.toDoRow, style.row)}
+          >Submit</button>
+        </form>
+        <ul className='ToDoUl' className={css(style.toDo)}>
+               {this.paginateToDo.bind(this)(todo.todo.edges)}
+        </ul>
+        <div className={css(style.row)}>
+          <button 
+            className={css(style.pagBtn)} 
+            onClick={this.back.bind(this)}>Back
+          </button>
+          <strong
+            className={css(style.pagBtn)} 
+          >
+            Page: {this.state.page}
+          </strong>
+          <button 
+            className={css(style.pagBtn)} 
+            onClick={this.forward.bind(this)}>Forward
+          </button>
+        </div>
+     </div>
   )
   }
   loadMore(){
-    this.props.relay.loadMore(this.state.count, e => console.log(e))
+    this.props.relay.loadMore(this.state.count)
   }
 }
 
@@ -112,8 +131,6 @@ const ToDoList = createPaginationContainer(
   {
     direction: 'forward',
     getConnectionFromProps(props) {
-      console.log('getConnectionFromProps');
-      console.log(props);
       return props.todo && props.todo.todo;
     },
     getFragmentVariables(preVars, totalCount) {
@@ -154,6 +171,4 @@ const queryRenderer = ({city_id}) => {
  
   );
 };
-
 export default queryRenderer;
-
--- src/ToDoList/style.js
+++ src/ToDoList/style.js
@@ -1,4 +1,45 @@
 import { flatten } from '../../style/';
+import { StyleSheet } from 'aphrodite';
+
+export const style = StyleSheet.create({
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  pagBtn: {
+    display: 'flex',
+    margin: '1rem',
+    padding: '1rem',
+    flexGrow: 1,
+    flexShrink: 1
+  },
+  toDo: {
+    display: 'flex',
+    flexGrow: 1,
+    flexShrink: 1,
+    flexDirection: 'column',
+    padding: '0px',
+    color: 'white'
+  },
+  toDoAdd: {
+    display: 'flex',
+    flexGrow: 2,
+    flexShrink: 2,
+    color: '#FFFFFF',
+    flexDirection: 'column',
+  },
+  toDoRow: {
+    display: 'flex',
+    flexGrow: 1,
+    flexShrink: 1,
+    padding: '1px',
+    textAlign: 'center'
+  },
+  selfCenter: {
+   alignSelf: 'center'
+  }
+});
 
 export const ToDoStyle = {
   display: 'flex',
@@ -10,11 +51,10 @@ export const ToDoStyle = {
 
 export const ToDoAddStyle = {
   display: 'flex',
-  flexGrow: 1,
-  flexShrink: 1,
+  flexGrow: 2,
+  flexShrink: 2,
   color: '#FFFFFF',
   flexDirection: 'column',
-  height: '50%'
 };
 
 export const ToDoRowStyle = {
@@ -25,5 +65,3 @@ export const ToDoRowStyle = {
 }
 
 export const AddToTitle = flatten({alignSelf: 'center'}, ToDoRowStyle);
-export const AddToDoInput = flatten(ToDoRowStyle);
-export const AddToDoSubmit = flatten(ToDoRowStyle);
